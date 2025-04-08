7ea184b044ea8fc75a8f4ffaf991da0d8fc5a0e6
Firas kh <163108958+firas2200@users.noreply.github.com>
2025-04-08 14:29:49 +0100

    Delete front directory

--- front/Print/src/main/java/org/example/print/PartId/Entity.java
+++
@@ -1,4 +0,0 @@
-package org.example.print.PartId;
-
-public class Entity {
-}
--- front/sticker/src/main/java/org/example/kach/Repository/PartID/PartIDListingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.kach.Repository.PartID;
-
-import org.example.kach.Entity.PartId.PartIDListing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartIDListingRepository extends JpaRepository<PartIDListing, String> {
-}
